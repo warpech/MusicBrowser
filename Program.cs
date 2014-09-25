4dfdfba86ad1d18cd4e9c69456babbb4e3fa8d0b
Marcin Warpechowski <warpech@gmail.com>
2014-09-25 21:42:05 +0200

    iterate over a file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Xml;
 using System.Linq;
 using System.Xml.Linq;
 using System.IO;
+using System.Diagnostics;
 
 namespace MusicBrowser
 {
@@ -11,36 +12,30 @@ namespace MusicBrowser
     {
         static void Main()
         {
-            string markup =
-@"<Root>
-    <Child>
-        <GrandChild/>
-    </Child>
-</Root>";
-
             // Create a reader and move to the content.
-            using (XmlReader nodeReader = XmlReader.Create(new StringReader(markup)))
+            //using (XmlReader nodeReader = XmlReader.Create("\\psf\Home\Downloads\discogs_20140901_masters.xml"))
+            using (XmlReader nodeReader = XmlReader.Create("C:\\Starcounter Projects\\MusicBrowser\\data\\books.xml"))
             {
                 // the reader must be in the Interactive state in order to
                 // Create a LINQ to XML tree from it.
                 nodeReader.MoveToContent();
 
                 XElement xRoot = XElement.Load(nodeReader, LoadOptions.SetLineInfo);
-                Console.WriteLine("{0}{1}{2}",
+                Debug.WriteLine("{0}{1}{2}",
                     "Element Name".PadRight(20),
                     "Line".PadRight(5),
                     "Position");
-                Console.WriteLine("{0}{1}{2}",
+                Debug.WriteLine("{0}{1}{2}",
                     "------------".PadRight(20),
                     "----".PadRight(5),
                     "--------");
                 foreach (XElement e in xRoot.DescendantsAndSelf())
-                    Console.WriteLine("{0}{1}{2}",
+                    Debug.WriteLine("{0}{1}{2}",
                         ("".PadRight(e.Ancestors().Count() * 2) + e.Name).PadRight(20),
                         ((IXmlLineInfo)e).LineNumber.ToString().PadRight(5),
                         ((IXmlLineInfo)e).LinePosition);
             }
-            Environment.Exit(1);
+            Environment.Exit(0);
         }
     }
 }
