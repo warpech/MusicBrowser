aadec47ef86e93166a23934f4da17acf6643c568
Marcin Warpechowski <warpech@gmail.com>
2014-09-25 21:34:52 +0200

    iterate over XML string

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-using System;
+﻿using System;
 using Starcounter;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
 
 namespace MusicBrowser
 {
@@ -7,7 +11,36 @@ namespace MusicBrowser
     {
         static void Main()
         {
+            string markup =
+@"<Root>
+    <Child>
+        <GrandChild/>
+    </Child>
+</Root>";
 
+            // Create a reader and move to the content.
+            using (XmlReader nodeReader = XmlReader.Create(new StringReader(markup)))
+            {
+                // the reader must be in the Interactive state in order to
+                // Create a LINQ to XML tree from it.
+                nodeReader.MoveToContent();
+
+                XElement xRoot = XElement.Load(nodeReader, LoadOptions.SetLineInfo);
+                Console.WriteLine("{0}{1}{2}",
+                    "Element Name".PadRight(20),
+                    "Line".PadRight(5),
+                    "Position");
+                Console.WriteLine("{0}{1}{2}",
+                    "------------".PadRight(20),
+                    "----".PadRight(5),
+                    "--------");
+                foreach (XElement e in xRoot.DescendantsAndSelf())
+                    Console.WriteLine("{0}{1}{2}",
+                        ("".PadRight(e.Ancestors().Count() * 2) + e.Name).PadRight(20),
+                        ((IXmlLineInfo)e).LineNumber.ToString().PadRight(5),
+                        ((IXmlLineInfo)e).LinePosition);
+            }
+            Environment.Exit(1);
         }
     }
 }
